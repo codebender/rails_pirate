82b105d607e16e21e218289a4ab700f0363c35bd
Matthew Bender <benderm@gmail.com>
2014-10-25 16:34:43 -0600

    double rserve connection

--- spec/models/r_predictor_spec.rb
+++ spec/models/r_predictor_spec.rb
@@ -4,8 +4,9 @@ describe RPredictor do
   describe 'make_prediction' do
     it 'calls the rserve client with the r script' do
       instance = RPredictor.new([], [])
-      expect_any_instance_of(Rserve::Connection).to receive(:eval).
-        with(instance.send(:r_script))
+      rserve = double(Rserve::Connection)
+      allow(Rserve::Connection).to receive(:new).and_return(rserve)
+      expect(rserve).to receive(:eval).with(instance.send(:r_script))
 
       instance.make_prediction
     end
